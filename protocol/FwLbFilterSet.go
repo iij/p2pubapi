685a163f3c9cb941315c5f1183cf444385dd7f5a
Keisuke Taguchi <keisukt@gmail.com>
2020-08-25 12:22:33 +0900

    FilterRuleList is mandatory (#30)
    Co-authored-by: keisuk-t <keisuk-t@iij.ad.jp>

--- protocol/FwLbFilterSet.go
+++ protocol/FwLbFilterSet.go
@@ -5,12 +5,12 @@ import (
 )
 
 type FilterRule struct {
-	SourceNetwork      string `json:",omitempty"`
-	DestinationNetwork string `json:",omitempty"`
-	DestinationPort    string `json:",omitempty"`
-	Protocol           string `json:",omitempty"`
-	Action             string `json:",omitempty"`
-	Label              string `json:",omitempty"`
+	SourceNetwork      string
+	DestinationNetwork string
+	DestinationPort    string
+	Protocol           string
+	Action             string
+	Label              string
 }
 
 // FwLbFilterSet フィルタリングルール一括設定 (同期)
@@ -20,7 +20,7 @@ type FwLbFilterSet struct {
 	IflServiceCode string       `json:"-"` // FW+LB 専有タイプのサービスコード(ifl########)
 	IpVersion      string       `json:"-"` // IPv4ルールかIPv6ルール（V4, V6）
 	Direction      string       `json:"-"` // 入力方向（ExternalからInternalへ）のルールか出力方向のルールか。IPv6は"in"のみ指定可能（in, out）
-	FilterRuleList []FilterRule `json:",omitempty"`
+	FilterRuleList []FilterRule
 }
 
 // URI /:GisServiceCode/fw-lbs/:IflServiceCode/filters/:IpVersion/:Direction.json
